2102c86d60b5f110d0b42646983bcfd5f41940fd
rochapbernardo <bernardopolese2809@gmail.com>
2025-06-22 13:05:15 -0300

    refactor codeTranspiler.py

--- codeTranspiler.py
+++ codeTranspiler.py
@@ -24,22 +24,39 @@ class CodeTranspiler(Interpreter):
         self.output.append(f"{ident}{line}")
 
     def program(self, tree):
-        self.emit("#include <stdio.h>")
+        self.emit("#include <stdio.h>\n")
         self.visit_children(tree)
 
     def function(self, tree):
         name, parameters, fn_type, *block = tree.children
         #prepocess
         name = name.value
-        parameters = self.visit_children(parameters)
+        parameters = self.visit_children(parameters)[0]
         fn_type = self.visit_children(fn_type)[0]
 
+        #emit function
+        self.emit(f"{fn_type} {name}({self.parameters_to_str(parameters)}) {{")
+        self.indent_level += 1
+
+        #emit block
         for blocks in block:
             self.visit_children(blocks)
 
-        print(parameters)
+        self.indent_level -= 1
+        self.emit(f"}}")
 
-        exit(0)
+        #print(parameters)
+        #exit(0)
+
+    def parameters_to_str(self, parameters):
+        parameters_str = ""
+        
+        for i, p in enumerate(parameters):
+            parameters_str += f"{p['type']} {p['id']}"
+            if i+1 != len(parameters):
+                parameters_str += ","
+
+        return parameters_str
 
     def parameters_def_list(self, tree):
         return self.visit_children(tree)
