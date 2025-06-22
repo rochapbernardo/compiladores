205fc3bfa3699c14e7f20e5e2c4ebf6b42286aa5
André Luís <zandreluisos@gmail.com>
2025-06-22 18:44:40 -0300

    Remove old code

--- codeTranspiler.py
+++ codeTranspiler.py
@@ -21,27 +21,13 @@ class CodeTranspiler(Interpreter):
         import_statement = f"#include <{module}>"
         self.imports.append(import_statement)
 
-    def function(self, tree):
-        name, parameters, fn_type, *block = tree.children
-        #prepocess
-        name = name.value
-        parameters = self.visit_children(parameters)[0]
-        fn_type = self.visit_children(fn_type)[0]
     def transpile(self, ast):
         """Starts the transpilation process."""
         self.visit(ast)
         return f"{'\n'.join(self.imports)}\n\n{'\n'.join(self.output)}"
 
-        #emit function
-        self.emit(f"{fn_type} {name}({self.parameters_to_str(parameters)}) {{")
-        self.indent_level += 1
 
-        #emit block
-        for blocks in block:
-            self.visit_children(blocks)
 
-        self.indent_level -= 1
-        self.emit(f"}}")
     # Início
     # self.visit(ast) chama este método
     def program(self, tree) -> None:
@@ -49,28 +35,8 @@ class CodeTranspiler(Interpreter):
         self.emit_import("stdio.h")
         self.visit_children(tree)
 
-        #print(parameters)
-        #exit(0)
 
-    def parameters_to_str(self, parameters):
-        parameters_str = ""
-        
-        for i, p in enumerate(parameters):
-            parameters_str += f"{p['type']} {p['id']}"
-            if i+1 != len(parameters):
-                parameters_str += ","
 
-        return parameters_str
 
-    def parameters_def_list(self, tree):
-        return self.visit_children(tree)
-    
-    def parameter_def(self, tree):
-        return {
-            'type': self.visit_children(tree.children[0])[0].value,
-            'id': tree.children[1].value
-        }
 
-    def type(self, tree):
-        return tree.children[0].value
 
